9daaf01a191ed34a80dae7f0b01a94e6ecbc8f54
Reynaldo <reynaldo119@gmail.com>
2019-11-25 17:31:12 -0600

    Mitad de proyecto

--- OrganizadorSwift/OrganizadorSwift/ViewController.swift
+++ OrganizadorSwift/OrganizadorSwift/ViewController.swift
@@ -8,8 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var lista = ["tarea 1", "tarea 2", "tarea 3"]
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lista.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "tarea")
+        cell.textLabel?.text = lista[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
+        if editingStyle == UITableViewCell.EditingStyle.delete{
+            lista.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        let tab = tabBarController?.viewControllers![1] as! AgregarViewController
+        tab.index = indexPath.row
+        tabBarController?.selectedIndex = 1
+    }
+
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
