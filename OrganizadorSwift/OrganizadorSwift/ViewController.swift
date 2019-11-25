77a4ab05f2e4a9163261284947275fead2b22257
CEDAM18 <cedam18@MacBook-Pro-de-CDAM18.local>
2019-11-25 15:51:02 -0600

    Primer commit

---
+++ OrganizadorSwift/OrganizadorSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrganizadorSwift
+//
+//  Created by CEDAM18 on 25/11/19.
+//  Copyright © 2019 CEDAM18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
